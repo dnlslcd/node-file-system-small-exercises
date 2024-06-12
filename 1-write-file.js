8a298e89808318ae98173fdf9a8d09e977a99ee5
dnlslcd <danielsalcedoq@gmail.com>
2024-06-12 19:02:38 +0200

    1 - hecho

--- 1-write-file.js
+++ 1-write-file.js
@@ -2,3 +2,12 @@
  * Escribe un fichero de nombre "test.txt" con el texto "Hello World!".
  * 
  */
+
+const fs = require('fs');
+
+fs.writeFile("test.txt", "hi girl", (err) => {
+    if (err) {
+        console.log("Ha ocurrido un error: ", err);
+    }
+    console.log('File written successfully!');
+})
