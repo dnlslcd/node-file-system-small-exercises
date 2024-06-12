66a7a7d32f84794e96e6fb5ca6d29b896781e4e8
dnlslcd <danielsalcedoq@gmail.com>
2024-06-12 19:32:15 +0200

    5 - empezado

--- 5-read-directory.js
+++ 5-read-directory.js
@@ -4,3 +4,17 @@
  * REsultado esperado: https://oscarm.tinytake.com/msc/OTcyMDU5MV8yMzM1NjMzNA
  * 
  */
+
+const fs = require('fs');
+const directory = "./5-read-directory";
+
+fs.readdir(directory, (err, files) => {
+    if (err){
+        console.log("ERROR: ", err);
+        return;
+    }
+
+    files.forEach((file) => {
+        console.log("Archivos en el directorio: ", file);
+    });
+});
